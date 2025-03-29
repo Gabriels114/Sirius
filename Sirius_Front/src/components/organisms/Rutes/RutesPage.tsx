6d2f64c6aa5e3970dea49d6e37f0dcef5702653c
Yael <rayacova37@gmail.com>
2025-03-29 11:07:01 -0600

    feat: update RutesPage layout with card_container for improved display of routes

--- Sirius_Front/src/components/organisms/Rutes/RutesPage.tsx
+++ Sirius_Front/src/components/organisms/Rutes/RutesPage.tsx
@@ -7,9 +7,15 @@ export default function Rutes(){
         <section className="rutes_container">
             <SubTitle title="Rutas Predifinidas"/>
 
-            <div className="Card_rutes">
-                <h3>Ruta: A</h3>
-                <div><WatchIcon color="var(--text-subtitles-100)" sx={10}/> 45 min</div>
+            <div className="card_container">
+                <div className="Card_rutes">
+                    <h3>Ruta: A</h3>
+                    <div><WatchIcon color="var(--text-subtitles-100)" sx={10}/> 45 min</div>
+                </div>
+                <div className="Card_rutes">
+                    <h3>Ruta: B</h3>
+                    <div><WatchIcon color="var(--text-subtitles-100)" sx={10}/> 35 min</div>
+                </div>
             </div>
         </section>
     )
